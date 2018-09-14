7805a758ac3f1a23e9f2e0a7ef241163772d0dda
kes3583 <kes3583@gmail.com>
2018-09-14 11:39:29 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -5,25 +5,49 @@
 /**
    * Selectors
    */
-  var container = document.querySelector('.container');
+
+  //const audio = document.querySelector(`audio[data-key=${e.keyCode}]`);
+  const keyContainer = document.querySelector('.key-container')
+  const keys= keyContainer.querySelectorAll('div');
+  const audioContainer = document.querySelector('.audio-container')
+
+
 
 /**
    * Methods
    */
 
-  //load callback
-  var textAnimation = function (el) {
 
-    this.el = el;
-    console.log(this.el);
+  //key Events
+window.addEventListener('keydown',function(e){
+      const keycode = e.which || e.code; // cross-broswer
+      const audio = audioContainer.querySelector(`audio[data-key='${keycode}']`);
+      const key = keyContainer.querySelector(`div[data-key='${keycode}']`);
 
-  }
+
+    if(!audio) return; // if other keys are down, exits this function.
+    //console.log(audio)
+    //console.log(key)
+
+    // for (var i = 0; i < keys.length; i++) {
+    //     keys[i].classList.remove('active');
+    // }
+    key.classList.add('active')
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+
+
+  })
+//console.log(keys);
+//keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
 
 /**
    * Events/APIs/init
    */
 
-  var text_ = new textAnimation(container);
+  //var text_ = new textAnimation(container);
 
 
 
