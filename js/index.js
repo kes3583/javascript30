4082ab2116f7484d2035bb929e77e4d1c3bd0296
kes3583 <kes3583@gmail.com>
2018-09-14 23:32:15 +0200

    last commit

--- js/index.js
+++ js/index.js
@@ -18,28 +18,33 @@
    */
 
 
-  //key Events
-window.addEventListener('keydown',function(e){
-      const keycode = e.which || e.code; // cross-broswer
-      const audio = audioContainer.querySelector(`audio[data-key='${keycode}']`);
-      const key = keyContainer.querySelector(`div[data-key='${keycode}']`);
+    const removeTransition = function (e) {
+        //console.log(e.propertyName) // check all the transition effects
+        if(e.propertyName !== 'transform') return; // skip it if it's not a transform, if there is transfrom effect, run the code below. 
+        //console.log(this)
+        //console.log(e.propertyName)
+        this.classList.remove('active');
+    }
+    const keySounds = function (e){
+          const keycode = e.which || e.code; // cross-broswer
+          const audio = audioContainer.querySelector(`audio[data-key='${keycode}']`);
+          const key = keyContainer.querySelector(`div[data-key='${keycode}']`);
+
+        if(!audio) return; // if other keys are down, exits this function.
+
+        // for (var i = 0; i < keys.length; i++) {
+        //     keys[i].classList.remove('active');
+        // } //remove active class before keydown
 
 
-    if(!audio) return; // if other keys are down, exits this function.
-    //console.log(audio)
-    //console.log(key)
-
-    // for (var i = 0; i < keys.length; i++) {
-    //     keys[i].classList.remove('active');
-    // }
-    key.classList.add('active')
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
+        audio.currentTime = 0; // rewind to the start
+        audio.play();
+        key.classList.add('active')
 
 
-  })
-//console.log(keys);
-//keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+      }
+      //key Events
+
 
 
 
@@ -47,8 +52,13 @@ window.addEventListener('keydown',function(e){
    * Events/APIs/init
    */
 
-  //var text_ = new textAnimation(container);
 
+   window.addEventListener('keydown', keySounds)
+   //console.log(keys);
+   //keys.forEach(el => el.addEventListener('transitionend', removeTransition));
+   keys.forEach(function(el){
+       el.addEventListener('transitionend', removeTransition)
+   });
 
 
 })(window, document);
