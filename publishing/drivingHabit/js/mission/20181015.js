6cdc7b06a2ab0220ef7931fd566a169c072be825
kes3583 <kes3583@gmail.com>
2019-07-21 22:07:03 +0900

    mission

--- publishing/drivingHabit/js/mission/20181015.js
+++ publishing/drivingHabit/js/mission/20181015.js
@@ -1,20 +1,20 @@
 
 $(function(){
 	//이미지 프리로딩
-	function preloading (imageArray) 
-	{ 
-		let n = imageArray.length; 
-		for (let i = 0; i < n; i++) { 
-			let img = new Image(); 
-			img.src = imageArray[i]; 
-		} 
+	function preloading (imageArray)
+	{
+		let n = imageArray.length;
+		for (let i = 0; i < n; i++) {
+			let img = new Image();
+			img.src = imageArray[i];
+		}
 	}
 
-	preloading([ './images/mission/20181015/images/main_ani_01.gif', 
-		'./images/mission/20181015/main_ani_02.gif', 
+	preloading([ './images/mission/20181015/images/main_ani_01.gif',
+		'./images/mission/20181015/main_ani_02.gif',
 		'./images/mission/20181015/main_ani_03.gif'
 		]);
-	
+
 	var score =80;
 	var barColor="#fa5653";
 	var bgColor="#e5e5e5";
@@ -48,45 +48,50 @@ $(function(){
 		    	$(".myPosition").show();
 				$(".zeroTxt").hide();
 			    if(score < 30){
-				barColor="#fa5653";	
+				barColor="#fa5653";
 		    	$(".text").addClass("col01");
 			    }
 			    else if(score <80){
-					barColor="#ebba2d";		
+					barColor="#ebba2d";
 					$(".text").addClass("col02");
 				}else if(score <= 100){
-					barColor="#24c89f";		
+					barColor="#24c89f";
 					$(".text").addClass("col03");
 				}
 		     }
-			
+
 		}
 		getStatusColor(score);
-		
+
 		$(".circle canvas").attr("width", 240).attr("height",130);
 		$(".circle canvas").gauge(score,{color: barColor, bgcolor: bgColor, lineWidth :9, type:"halfcircle"});
-		
+
 	}
-	
+
 	   scoreGraph();
 	   var gageClear=0;
 	   $(window).scroll(function(){
 		 var now = $(this).scrollTop();
 		 var mid=$(".mid");
+		 var elem = document.querySelector('.mid');
 		   if(gageClear == 0 && now >= (mid.offset().top  - $(this).height() + mid.outerHeight())){
+			   console.log('window.scrollY',window.scrollY)
+			   console.log('mid.offset().top',elem.offsetTop)
+			   console.log('$(this).height()',window.innerHeight)
+			   console.log('mid.outerHeight()',elem.offsetHeight)
 			   if(gageClear == 0){
-				   gageClear = 1; 
+				   gageClear = 1;
 				   scoreGraph();
 			   }
-			 
+
 		   }
-		   
+
 	   });
 	   $(".circle").click(function(){
 		   scoreGraph();
-			
+
 			});
-	   
+
 	var swiper = new Swiper(".swiperWrap .swiper-container",{
 		slidesPerView: 1.23,
         centeredSlides: true,
@@ -97,7 +102,7 @@ $(function(){
 	        disableOnInteraction: false,
 	    }
 	 });
-	   
+
 	 swiper.on('transitionStart', function () {
 		var index = swiper.activeIndex;
 		 var src="./images/mission/20181015/main_ani_0"+(index+1)+".gif"
@@ -115,8 +120,8 @@ $(function(){
 		        delay: 1800,
 		        disableOnInteraction: false,
 		    }
-		});	
-		
+		});
+
     $(".shareBtn").click(function(){
         //$(".shareBtns").addClass("show");
         location.href = "http://minasis01.dothome.co.kr/comm/missions/mission_20180514/index.html"
